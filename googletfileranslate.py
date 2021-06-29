d948d629e812a0836fde32fb10e112f81d96d8c7
kota makino <kota.m.2774@gmail.com>
2021-06-29 09:18:04 +0900

    日本語ファイルを英語に訳す(趣味)

---
+++ googletfileranslate.py
@@ -0,0 +1,19 @@
+from googletrans import Translator
+import sys
+
+args= sys.argv
+if len(args) < 2:
+    print('Command should be like')
+    print('python3 googlefiletranlate.py textfile.txt')
+else:
+    with open(args[1], encoding='utf-8_sig') as f:
+      print(f)
+      lines = f.readlines()
+
+    translator = Translator()
+    for line in lines:
+        translated = translator.translate(line, dest="ja");
+        print(line)
+        print(translated.text)
+        print()
+    print('finished')
